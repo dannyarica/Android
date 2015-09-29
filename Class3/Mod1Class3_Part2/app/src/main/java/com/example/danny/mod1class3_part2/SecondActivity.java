f7ad01a0257343e64974109e26c0de2cb7eeb73f
Danny Arica <danny.arica@cignium.com>
2015-09-28 22:04:23 -0500

    Navigation between activities.

---
+++ Class3/Mod1Class3_Part2/app/src/main/java/com/example/danny/mod1class3_part2/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.example.danny.mod1class3_part2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by DArica on 28/09/2015.
+ */
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
